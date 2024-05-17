80ddebf440cf7c6c6686190943a0cdda144cdd42
KamisamaCoder <pajingkamin123@gmail.com>
2024-05-17 17:25:48 +0530

    updated the project

--- landmarks.py
+++ landmarks.py
@@ -8,7 +8,7 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.5)
-data_dir = "./data"
+data_dir = "./data_small"
 data = []
 labels = []
 
@@ -16,6 +16,9 @@ for dir_ in os.listdir(data_dir):
     for img_path in os.listdir(os.path.join(data_dir, dir_)):
         data_aux = []
         img = cv2.imread(os.path.join(data_dir, dir_, img_path))
+        if img is None:
+            print(f"Error reading image: {os.path.join(data_dir, dir_, img_path)}")
+            continue
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(img_rgb)
         if results.multi_hand_landmarks:
@@ -27,6 +30,6 @@ for dir_ in os.listdir(data_dir):
                     data_aux.append(y)
             data.append(data_aux)
             labels.append(dir_)
-file = open("data.pickle", "wb")
+file = open("data_small.pickle", "wb")
 pickle.dump({"data": data, "labels": labels}, file)
 file.close()
--- train_model.py
+++ train_model.py
@@ -7,17 +7,29 @@ from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
 
-data_dict = pickle.load(open("./data.pickle", "rb"))
+# data_dict = pickle.load(open("./data_small.pickle", "rb"))
+# data = data_dict["data"]
+
+# # Determine the length of the longest sequence
+# max_length = max(len(seq) for seq in data)
+
+# # Pad the sequences
+# data = pad_sequences(data, maxlen=max_length, padding="post")
+
+
+data_dict = pickle.load(open("./processed_data.pickle", "rb"))
 data = np.asarray(data_dict["data"])
 labels = np.asarray(data_dict["labels"])
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+x_train, x_test, y_train, y_test = train_test_split(
+    data, labels, test_size=0.2, shuffle=True, stratify=labels
+)
 model = RandomForestClassifier()
 model.fit(x_train, y_train)
 y_predict = model.predict(x_test)
 score = accuracy_score(y_predict, y_test)
 
 print("{}% classified correctly".format(score * 100))
-f = open("model.p", "wb")
+f = open("data_small.p", "wb")
 pickle.dump({"model": model}, f)
 f.close()
 
